0bf5ad5230d7a36e35b495fb0b79d50fd1a8d841
Tamas Tobi <tamas.tobi@realeyesit.com>
2016-02-21 21:17:17 +0100

    Caching ON

--- httproot/mimoxindex.bottleUI_v3.py
+++ httproot/mimoxindex.bottleUI_v3.py
@@ -51,7 +51,7 @@ ADMIN_TEMPLATE=os.environ.get('ADMIN_TEMPLATE')
 UPLOADS=os.environ.get('UPLOADS')
 CACHE=os.environ.get('CACHE')
 CACHE_TIME=3600 #1h
-CACHE_USE=False
+CACHE_USE=True
 
 TERM_EMAIL_RECPS=['tamas.tobi@gmail.com','mimoxindex@mimox.com']
 ##########################################
